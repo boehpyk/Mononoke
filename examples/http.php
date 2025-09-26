fce8dcddd1cebaa63e883eac1ffc94c6ff3e42e7
Henric Johansson <hej@henricjohansson.se>
2025-09-26 22:25:57 +0200

    chore: update http example to use config

--- examples/http.php
+++ examples/http.php
@@ -5,9 +5,19 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp\Psr7\Response as Psr7Response;
+use Kekke\Mononoke\Attributes\Config;
 use Kekke\Mononoke\Attributes\Http;
+use Kekke\Mononoke\Models\AwsConfig;
+use Kekke\Mononoke\Models\HttpConfig;
+use Kekke\Mononoke\Models\MononokeConfig;
 use Kekke\Mononoke\Service as MononokeService;
 
+
+#[Config(
+    mononokeConfig: new MononokeConfig(numberOfTaskWorkers: 5),
+    awsConfig: new AwsConfig(sqsPollTimeInSeconds: 10),
+    httpConfig: new HttpConfig(port: 8080),
+)]
 class Service extends MononokeService
 {
     #[Http('GET', '/health')]
